e313a82fe74459aedba22f164309df9a5d7162a9
idk <r.carlos@correo.unimet.edu.ve>
2024-11-04 21:23:31 -0400

    inicializacion de modulos

---
+++ Arbol Genealógico/src/LinealStructures/List.java
@@ -0,0 +1,167 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LinealStructures;
+
+/**
+ *
+ * @author carlosrodriguez
+ */
+
+/**
+ *
+ * @author carlosrodriguez
+ * @param <T>
+ */
+public class List<T> {
+
+    private Node pFirst;
+    private int size;
+
+    public List() {
+        this.pFirst = null;
+        this.size = 0;
+    }
+
+    public Node getpFirst() {
+        return pFirst;
+    }
+
+    public void setpFirst(Node pFirst) {
+        this.pFirst = pFirst;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean isEmpty() {
+        if (this.getpFirst() == null) {
+            return true;
+        }
+        return false;
+    }
+    
+    public void destructor(){
+        if(!isEmpty()){
+            this.setpFirst(null);
+            this.setSize(0);
+        }
+    }
+
+    public Node last() {
+        if (!isEmpty()) {
+            Node aux = this.getpFirst();
+            while (aux.getpNext() != null) {
+                aux = aux.getpNext();
+            }
+            return aux;
+        }
+        return null;
+    }
+
+    public void add(T newData) {
+        Node newNode = new Node(newData);
+        if (!isEmpty()) {
+            Node lastNode = last();
+            lastNode.setpNext(newNode);
+
+        } else {
+            this.setpFirst(newNode);
+        }
+        this.setSize(this.getSize() + 1);
+    }
+
+    public void newFirst(T data) {
+        Node newNode = new Node(data);
+        if (!isEmpty()) {
+            Node currentFirst = this.getpFirst();
+            this.setpFirst(newNode);
+            newNode.setpNext(currentFirst);
+        } else {
+            this.setpFirst(newNode);
+        }
+        this.setSize(this.getSize() + 1);
+    }
+    
+    public void delete(){
+        if(this.getSize() == 1){
+            destructor();
+        }
+        else if(!isEmpty()){
+            Node last = last();
+            Node aux = this.getpFirst();
+            while (aux.getpNext() != last) {
+                aux = aux.getpNext();
+            }
+            aux.setpNext(null);
+            this.setSize(this.getSize() - 1);
+
+        }
+    }
+    
+    public void deleteFirst(){
+        if (!isEmpty()) {
+            Node oldFirst = this.getpFirst();
+            this.setpFirst(oldFirst.getpNext());
+            oldFirst.setpNext(null);
+            this.setSize(this.getSize() - 1);
+        }
+    }
+    
+    public void deleteByIndex(int index) {
+        if (index < 0 || index >= this.getSize()) {
+            return;
+        } else if (index == 0) {
+            deleteFirst();
+        } else if (index == this.getSize()-1) {
+            delete();
+        } else {
+            Node aux = this.getpFirst();
+            for (int i = 0; i < index-1; i++) {
+                aux = aux.getpNext();
+            }
+            Node deleteable = aux.getpNext();
+            aux.setpNext(deleteable.getpNext());
+            deleteable.setpNext(null);
+            this.setSize(this.getSize() - 1);
+        }
+    }
+
+    public void addByIndex(int index, T data) {
+        if (index < 0 || index > this.getSize()) {
+            return;
+        } else if (index == 0) {
+            newFirst(data);
+        } else {
+            Node newNode = new Node(data);
+            Node aux = this.getpFirst();
+            for (int i = 0; i < index - 1; i++) {
+                aux = aux.getpNext();
+            }
+            Node next = aux.getpNext();
+            aux.setpNext(newNode);
+            newNode.setpNext(next);
+            this.setSize(this.getSize() + 1);
+        }
+
+    }
+
+    public void show() {
+        if (!isEmpty()) {
+            Node aux = this.getpFirst();
+            while (aux != null) {
+                System.out.println(aux.getData());
+                aux = aux.getpNext();
+            }
+        } else {
+            System.out.println("None");
+        }
+    }
+}
+
---
+++ Arbol Genealógico/src/LinealStructures/Node.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LinealStructures;
+
+/**
+ *
+ * @author carlosrodriguez
+ */
+public class Node<T> {
+    private Node pNext;
+    private T data;
+
+    public Node(T data) {
+        this.pNext = null;
+        this.data = data;
+    }
+
+    public Node getpNext() {
+        return pNext;
+    }
+
+    public void setpNext(Node pNext) {
+        this.pNext = pNext;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+    
+    
+    
+    
+}
