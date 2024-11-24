0cb8c19c6356baa210ea89ff58cd60adfe11506b
Pedrosebastiano <pedro.sebastiano@correo.unimet.edu.ve>
2024-11-23 21:51:50 -0400

    Resolviendo conflictos entre master y main

--- Arbol Genealógico/src/EDD/List.java
+++ Arbol Genealógico/src/EDD/List.java
@@ -3,43 +3,66 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package EDD;
-
 /**
+ * Representa una lista enlazada.
  *
- * @author carlosrodriguez
- */
-
-/**
- *
- * @author carlosrodriguez
- * @param <T>
+ * @param <T> el tipo de dato almacenado en la lista
+ * @Carlos Rodriguez
  */
 public class List<T> {
 
     private Node<T> pFirst;
     private int size;
 
+    /**
+     * Construye una nueva lista vacía.
+     */
     public List() {
         this.pFirst = null;
         this.size = 0;
     }
 
+    /**
+     * Devuelve el primer nodo de la lista.
+     * 
+     * @return el primer nodo
+     */
     public Node<T> getpFirst() {
         return pFirst;
     }
 
+    /**
+     * Establece el primer nodo de la lista.
+     * 
+     * @param pFirst el primer nodo
+     */
     public void setpFirst(Node<T> pFirst) {
         this.pFirst = pFirst;
     }
 
+    /**
+     * Devuelve el tamaño de la lista.
+     * 
+     * @return el tamaño de la lista
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Establece el tamaño de la lista.
+     * 
+     * @param size el tamaño de la lista
+     */
     public void setSize(int size) {
         this.size = size;
     }
 
+    /**
+     * Verifica si la lista está vacía.
+     * 
+     * @return true si la lista está vacía, de lo contrario false
+     */
     public boolean isEmpty() {
         if (this.getpFirst() == null) {
             return true;
@@ -47,6 +70,9 @@ public class List<T> {
         return false;
     }
     
+    /**
+     * Destruye la lista, eliminando todos sus elementos.
+     */
     public void destructor(){
         if(!isEmpty()){
             this.setpFirst(null);
@@ -54,6 +80,11 @@ public class List<T> {
         }
     }
 
+    /**
+     * Devuelve el último nodo de la lista.
+     * 
+     * @return el último nodo, o null si la lista está vacía
+     */
     public Node<T> last() {
         if (!isEmpty()) {
             Node<T> aux = this.getpFirst();
@@ -65,18 +96,27 @@ public class List<T> {
         return null;
     }
 
+    /**
+     * Agrega un nuevo nodo con los datos proporcionados al final de la lista.
+     * 
+     * @param newData los datos a ser almacenados en el nuevo nodo
+     */
     public void add(T newData) {
         Node<T> newNode = new Node<>(newData);
         if (!isEmpty()) {
             Node<T> lastNode = last();
             lastNode.setpNext(newNode);
-
         } else {
             this.setpFirst(newNode);
         }
         this.setSize(this.getSize() + 1);
     }
 
+    /**
+     * Agrega un nuevo nodo con los datos proporcionados al principio de la lista.
+     * 
+     * @param data los datos a ser almacenados en el nuevo nodo
+     */
     public void newFirst(T data) {
         Node<T> newNode = new Node<>(data);
         if (!isEmpty()) {
@@ -88,12 +128,14 @@ public class List<T> {
         }
         this.setSize(this.getSize() + 1);
     }
-    
+
+    /**
+     * Elimina el último nodo de la lista.
+     */
     public void delete(){
         if(this.getSize() == 1){
             destructor();
-        }
-        else if(!isEmpty()){
+        } else if(!isEmpty()){
             Node<T> last = last();
             Node<T> aux = this.getpFirst();
             while (aux.getpNext() != last) {
@@ -101,10 +143,12 @@ public class List<T> {
             }
             aux.setpNext(null);
             this.setSize(this.getSize() - 1);
-
         }
     }
     
+    /**
+     * Elimina el primer nodo de la lista.
+     */
     public void deleteFirst(){
         if (!isEmpty()) {
             Node<T> oldFirst = this.getpFirst();
@@ -113,7 +157,12 @@ public class List<T> {
             this.setSize(this.getSize() - 1);
         }
     }
-    
+
+    /**
+     * Elimina el nodo en el índice especificado.
+     * 
+     * @param index el índice del nodo a eliminar
+     */
     public void deleteByIndex(int index) {
         if (index < 0 || index >= this.getSize()) {
             return;
@@ -133,6 +182,12 @@ public class List<T> {
         }
     }
 
+    /**
+     * Agrega un nuevo nodo con los datos proporcionados en el índice especificado.
+     * 
+     * @param index el índice donde agregar el nuevo nodo
+     * @param data los datos a ser almacenados en el nuevo nodo
+     */
     public void addByIndex(int index, T data) {
         if (index < 0 || index > this.getSize()) {
             return;
@@ -149,9 +204,11 @@ public class List<T> {
             newNode.setpNext(next);
             this.setSize(this.getSize() + 1);
         }
-
     }
 
+    /**
+     * Muestra los datos de todos los nodos en la lista.
+     */
     public void show() {
         if (!isEmpty()) {
             Node<T> aux = this.getpFirst();
@@ -160,8 +217,7 @@ public class List<T> {
                 aux = aux.getpNext();
             }
         } else {
-            System.out.println("None");
+            System.out.println("Ninguno");
         }
     }
 }
-
--- Arbol Genealógico/src/EDD/Node.java
+++ Arbol Genealógico/src/EDD/Node.java
@@ -5,35 +5,59 @@
 package EDD;
 
 /**
+ * Representa un nodo en una lista enlazada.
  *
- * @author carlosrodriguez
+ * @param <T> el tipo de dato almacenado en el nodo
+ * @Carlos Rodriguez
  */
 public class Node<T> {
     private Node pNext;
     private T data;
 
+    /**
+     * Construye un nuevo nodo con los datos proporcionados.
+     * 
+     * @param data los datos a ser almacenados en el nodo
+     */
     public Node(T data) {
         this.pNext = null;
         this.data = data;
     }
 
+    /**
+     * Devuelve el siguiente nodo en la lista.
+     * 
+     * @return el siguiente nodo
+     */
     public Node getpNext() {
         return pNext;
     }
 
+    /**
+     * Establece el siguiente nodo en la lista.
+     * 
+     * @param pNext el siguiente nodo
+     */
     public void setpNext(Node pNext) {
         this.pNext = pNext;
     }
 
+    /**
+     * Devuelve los datos almacenados en el nodo.
+     * 
+     * @return los datos
+     */
     public T getData() {
         return data;
     }
 
+    /**
+     * Establece los datos almacenados en el nodo.
+     * 
+     * @param data los datos
+     */
     public void setData(T data) {
         this.data = data;
     }
-    
-    
-    
-    
 }
+
